a59b2074db5be87485b9524ce1b52b4ab06fe1f5
MekalaSindhu <mekalasaisindhu@gmail.com>
2025-11-05 10:25:54 +0530

    main.tf

---
+++ main.tf
@@ -0,0 +1,46 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# --- IAM Role for Lambda ---
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "lambda_exec_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Effect = "Allow"
+        Sid    = ""
+      }
+    ]
+  })
+}
+
+# --- Attach Basic Execution Policy ---
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+# --- Create ZIP file for Lambda code ---
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_file = "${path.module}/lambda_function.py"
+  output_path = "${path.module}/lambda_function.zip"
+}
+
+# --- Lambda Function ---
+resource "aws_lambda_function" "hello_lambda" {
+  function_name = "hello_lambda"
+  role          = aws_iam_role.lambda_exec_role.arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.9"
+
+  filename         = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+}
